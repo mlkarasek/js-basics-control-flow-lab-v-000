cc90a151f20d766fda502c4e495fc31e40be9eda
Monica  Karasek <mlkarasek@gmail.com>
2018-09-02 05:27:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your code in this file!
+function scuberGreetingForFeet(someValue) {
+
+    let message
+    if (someValue < 400) {
+        message = 'This one is on me!'
+    }
+    if (someValue > 2000) {
+        message = 'I will gladly take your thirty bucks.'
+    }
+    if (someValue > 2500) {
+        message = 'No can do.'
+    }
+    return message
+}
+
+function ternaryCheckCity(city) {
+
+    let message;
+    city === 'NYC' ? message = 'Ok, sounds good.' : message = 'No go.';
+    return message
+}
+
+function switchOnCharmFromTip(tip) {
+
+    let message = 'Bye.';
+
+    switch (tip) {
+        case 'generous':
+            message = 'Thank you so much.';
+            break;
+        case 'not as generous':
+            message = 'Thank you.';
+            break;
+    }
+    return message
+}
